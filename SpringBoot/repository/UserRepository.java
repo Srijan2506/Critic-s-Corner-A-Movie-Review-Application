82ae903bba8b2f78271a1f659df7619d8a5220e5
Srijan2506 <155107972+Srijan2506@users.noreply.github.com>
2026-03-28 15:58:40 +0530

    Update UserRepository.java

--- SpringBoot/repository/UserRepository.java
+++ SpringBoot/repository/UserRepository.java
@@ -2,7 +2,7 @@ package com.example.srijanshukla.SpringBoot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.SpringBoot.entity.User;
+import com.example.srijanshukla.SpringBoot.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
