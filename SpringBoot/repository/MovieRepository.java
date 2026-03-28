a350f7bb055defc0ce435e7ac3abe2c31aaecb6d
Srijan2506 <155107972+Srijan2506@users.noreply.github.com>
2026-03-28 15:57:11 +0530

    Update MovieRepository.java

--- SpringBoot/repository/MovieRepository.java
+++ SpringBoot/repository/MovieRepository.java
@@ -4,7 +4,7 @@ package com.example.srijanshukla.SpringBoot.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.SpringBoot.entity.Moviedata;
+import com.example.srijanshukla.SpringBoot.entity.Moviedata;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Moviedata, Long> {
